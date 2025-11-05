04c1f306bed2a3d375dec430d9a6325b0c063407
Василиса <144157069+VasilisaFyod@users.noreply.github.com>
2025-11-06 01:30:24 +0300

    Add solution

---
+++ Lab/ArrayLibrary/CMas.cs
@@ -0,0 +1,45 @@
+﻿namespace Lab
+{
+    public class CMas
+    {
+        private int[] mas;
+
+        public int GetLengthMas
+        {
+            get { return mas.Length; }
+        }
+
+        public int this[int index]
+        {
+            get { return mas[index]; }
+            set { mas[index] = value; }
+        }
+
+        public CMas(int length)
+        {
+            mas = new int[length];
+        }
+
+        public CMas(int[] masValue)
+        {
+            mas = new int[masValue.Length];
+            Array.Copy(masValue, mas, masValue.Length);
+        }
+
+        public static int countPositiveNumbers(params CMas[] mass)
+        {
+            int totalCount = 0;
+            foreach (var cMas in mass)
+            {
+                foreach (int num in cMas.mas)
+                {
+                    if (num > 0)
+                    {
+                        totalCount++;
+                    }
+                }
+            }
+            return totalCount;
+        }
+    }
+}
---
+++ Lab/ArrayTests/UnitTests1.cs
@@ -0,0 +1,61 @@
+﻿using Lab;
+using Xunit;
+
+namespace Lab.ArrayTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CountPositiveNumbers_WithTwoMixedArrays_ReturnsCorrectCount()
+        {
+            // Arrange
+            CMas mas1 = new CMas(new int[] { 2, -2, 0, 0, 3 });
+            CMas mas2 = new CMas(new int[] { 0, -1, -2, 4, 5 });
+
+            // Act
+            int result = CMas.countPositiveNumbers(mas1, mas2);
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+
+        [Theory]
+        [InlineData(0, new int[] { }, new int[] { })]
+        public void CountPositiveNumbers_EmptyArrays_ReturnsZero(int expected, int[] masArray, int[] mas2Array)
+        {
+            // Arrange
+            CMas mas1 = new CMas(masArray);
+            CMas mas2 = new CMas(mas2Array);
+
+            // Act
+            int result = CMas.countPositiveNumbers(mas1, mas2);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> TestData =>
+        new List<object[]>
+        {
+            new object[] { 4, new int[] { 2, -2, 0, 0, 3 }, new int[] { 0, -1, -2, 4, 5 } },
+            new object[] { 0, new int[] { }, new int[] { } },
+            new object[] { 1, new int[] { 0 }, new int[] { 10, -10 } },
+            new object[] { 3, new int[] { 5, 6 }, new int[] { -1, -2, 3 } },
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void CountPositiveNumbers_VariousInputs_ReturnsCorrectCount(int expected, int[] masArray1, int[] masArray2)
+        {
+            // Arrange
+            CMas mas1 = new CMas(masArray1);
+            CMas mas2 = new CMas(masArray2);
+
+            // Act
+            int result = CMas.countPositiveNumbers(mas1, mas2);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
